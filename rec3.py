9716f848bb21591c7afdc2d65fecfc355675b2b0
h8ful <efslack2017@gmail.com>
2017-09-25 18:51:09 +0200

    cf not right, cell 257

--- rec3.py
+++ rec3.py
@@ -36,6 +36,7 @@ class Rec():
         self.rec_len = rec_len
 
         self.user_perf = dict()
+        self.rec_list_ = dict()
         self.test = test
         for u in self.targets:
             y_true = test[u].nonzero()[0]
@@ -43,11 +44,12 @@ class Rec():
             # if len(y_true) == 0: continue
 
             y_pred = self.recommendation_[u][:self.rec_len]
+            self.rec_list_[u] = y_pred
             right_rec = len(set(y_true).intersection(set(y_pred))) * 1.0
-            precision = right_rec / self.default_rec_len
+            precision = right_rec / self.rec_len
             # if user has no rating in test set, ignor him
             recall = right_rec / len(y_true)
-            self.user_perf[u] = (precision, recall)
+            self.user_perf[u] = (precision, recall, right_rec)
         self.precision_ = np.average([i[0] for i in self.user_perf.values()])
         self.recall_ = np.average([i[1] for i in self.user_perf.values()])
         return self.precision_, self.recall_
@@ -55,32 +57,34 @@ class Rec():
 from sklearn.metrics.pairwise import pairwise_distances
 
 class IBCF():
-    def __init__(self, sim, topN):
+    def __init__(self, sim):
         self.sim = sim
-        self.topN = topN
     def compute_similarity(self, profile):
         '''
 
         :param profile: each row is a profile vector, r*c size
         :return: similarity matrix, r*r size
         '''
-        self.profile = profile
         self.item_num_ = profile.shape[0]
-        self.similarities_ = np.zeros(self.item_num_)
+        self.similarities_ = np.zeros((self.item_num_, self.item_num_))
         if self.sim == 'dot':
             self.similarities_ = self.profile.dot(self.profile.T)
         else:
-            self.similarities_ = pairwise_distances(self.profile, metric=self.sim, n_jobs= 10)
+            # import pdb; pdb.set_trace()
+            self.similarities_ = 1.0 - pairwise_distances(self.profile, metric=self.sim, n_jobs= 4)
+        # set similarity to identity to 0
+        self.similarities_ = np.multiply(   self.similarities_, (1-np.eye(self.similarities_.shape[0])))
         return self
+
     def find_neighbors(self):
         self.item_neighbors_ = dict()
         for item in xrange(self.item_num_):
-            self.item_neighbors_[item] = np.argpartition(-1 * self.similarities_, self.topN)[:self.topN]
+            self.item_neighbors_[item] = np.argpartition(-1 * self.similarities_[item], self.topN)[:self.topN]
         return  self
 
     def score_pair_ib(self, user_id, item_id):
         neighborhood = self.item_neighbors_[item_id]
-        score = self.train_ratings[user_id, neighborhood].dot(self.similarities_[user_id,neighborhood])
+        score = self.train_ratings[user_id, neighborhood].dot(self.similarities_[item_id,neighborhood])
         return  score
 
     def fit(self,train_ratings, profile):
@@ -90,26 +94,35 @@ class IBCF():
         :return: self
         '''
         self.train_ratings = train_ratings
+        self.profile = profile
+
         self.compute_similarity(profile)
-        self.find_neighbors()
         return self
 
-    def compute_score(self, targets):
+    def compute_score(self,topN, targets):
+        self.topN = topN
+        self.find_neighbors()
+
         self.predicted_score_ = np.zeros(self.train_ratings.shape)
         for u in targets:
-            consumed = self.train_ratings[u, :].nonzero()
-            candinates = [i for i in self.item_neighbors_[k] for k in consumed]
-            for i in candinates:
-                self.predicted_score_[u, i] == self.score_pair_ib(user_id=u, item_id=i)
+            consumed = self.train_ratings[u, :].nonzero()[0]
+            # import pdb; pdb.set_trace()
+            candinates = [i for k in consumed for i in self.item_neighbors_[k] ]
+            for i in xrange(self.item_num_):
+            # for i in candinates:
+                self.predicted_score_[u, i] = self.score_pair_ib(user_id=u, item_id=i)
         return self
 
     def produce_reclist(self,targets):
-        self.compute_score(targets)
-        self.rec_ = Rec()
-        self.rec_.set_prediction_matrix(self.predicted_score_)
-        self.rec_.produce_rec_list(targets)
-        self.recommendations_ = self.rec_.recommendation_
-        return self
+        # predict score first, ensure self.predicted_score_ exsit
+        try:
+            self.rec_ = Rec()
+            self.rec_.set_prediction_matrix(self.predicted_score_)
+            self.rec_.produce_rec_list(targets)
+            self.recommendations_ = self.rec_.recommendation_
+            return self
+        except AttributeError:
+            raise AttributeError('compute score first')
 
     def evaluate(self, test, rec_len):
         self.test = test
